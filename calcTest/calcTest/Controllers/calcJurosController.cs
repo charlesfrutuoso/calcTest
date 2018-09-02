e00f2fcfac18db4f3189ab9eb33b47c2cac418eb
Charles Henrique Frutuoso <charleshenrique788@hotmail.com>
2018-09-02 12:28:02 -0300

    Versão 1 da calculadora de Juros

---
+++ calcTest/calcTest/Controllers/calcJurosController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace calcTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcJurosController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Valor inicial:" + Models.CalcJuros.valorInicial.ToString(),
+                                  "Valor final:" + Models.CalcJuros.valorFinal.ToString() };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post(decimal valorInicial, int tempoMeses)
+        {
+            try
+            {
+                Models.CalcJuros.valorInicial = valorInicial;
+                Models.CalcJuros.tempoMeses = tempoMeses;
+
+                /*  Valor Final = Valor Inicial * (1 + juros) ^ Tempo */
+                Models.CalcJuros.valorFinal = (Decimal)Math.Pow(Convert.ToDouble(Models.CalcJuros.valorInicial * (1 + (Models.CalcJuros.valorJuros / 100))), Convert.ToDouble(Models.CalcJuros.tempoMeses));
+
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+    }
+}
---
+++ calcTest/calcTest/Models/CalcJuros.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace calcTest.Models
+{
+    public static class CalcJuros
+    {
+        public static Decimal valorInicial { get; set; } = 0;
+        public static Decimal valorFinal { get; set; } = 0;
+        public static Int32 tempoMeses { get; set; } = 0;
+        public const Decimal valorJuros = 1;
+    }
+}
