c9aefb815c49d3cf2d48ec8b7fd60b2c331359cd
Charles Henrique Frutuoso <charleshenrique788@hotmail.com>
2018-09-03 19:37:16 -0300

    Inclusão do controller "showmethecode", com endpoint que exibe a URL do GITHub do projeto.

---
+++ calcTest/calcTest/Controllers/showmethecodeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace calcTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class showmethecodeController : ControllerBase
+    {
+        // GET: api/showmethecode
+        [HttpGet]
+        public string Get()
+        {
+            return "https://github.com/charlesfrutuoso/calcTest";
+        }
+
+        
+    }
+}
